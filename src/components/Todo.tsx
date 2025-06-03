9ac5aa045b5f5c165c8f5ca1eb5e90c6011fd155
D. Moschokarfi <despinamx16@gmail.com>
2025-06-03 19:50:51 +0300

    feat: added clear all button and counter for total, active and completed tasks

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -33,6 +33,8 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
                     ? {...todo, completed: !todo.completed}
                     : todo
             );
+        case "CLEAR_ALL":
+            return [];
         default:
             return state;
     }
@@ -41,9 +43,16 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
 const Todo = () =>{
     const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos);
 
+    const totalTasks : number = todos.length;
+    const completedTasks: number = todos.filter(t => t.completed).length;
+    const activeTasks: number = totalTasks - completedTasks;
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
+
+    const handleClearAll = () => {
+        dispatch({type: "CLEAR_ALL"});
+    }
 
     return (
         <>
@@ -51,6 +60,24 @@ const Todo = () =>{
                 <h1 className="text-center text-2xl mb-4">To-Do List</h1>
                 <TodoForm dispatch={dispatch} />
                 <TodoList todos={todos} dispatch={dispatch} />
+
+                { todos.length > 0 && (
+                    <>
+                        <div className="flex justify-between">
+                            <span>Total: {totalTasks}</span>
+                            <span>Active :{activeTasks}</span>
+                            <span>Completed: {completedTasks}</span>
+                        </div>
+                        <div className="text-end mt-4">
+                            <button
+                                onClick={handleClearAll}
+                                className="bg-cf-dark-red text-white py-2 px-4 rounded"
+                            >
+                                Clear all
+                            </button>
+                        </div>
+                    </>
+                )}
             </div>
         </>
     )
